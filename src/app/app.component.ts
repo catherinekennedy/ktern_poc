22ac854d7509f56b65114c55e7dc06bcde664191
catherinekennedy <67736813+catherinekennedy@users.noreply.github.com>
2022-09-01 18:51:07 +0530

    intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { GlobalConstants } from "src/app/common/globalvar";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   isCollapsed = false;
+  percent = GlobalConstants.percentage;
+  appTour = GlobalConstants.arrTour;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,38 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ShepsherdComponent } from "./shepsherd/shepsherd.component";
 import { ShepherdService } from "angular-shepherd";
-import { IntroComponent } from './intro/intro.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { SampleComponent } from './sample/sample.component';
-import { IconsProviderModule } from './icons-provider.module';
+import { IntroComponent } from "./intro/intro.component";
+import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { SampleComponent } from "./sample/sample.component";
+import { IconsProviderModule } from "./icons-provider.module";
+import { LoginComponent } from "./intro/login/login.component";
+import { HomeComponent } from "./intro/home/home.component";
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent, ShepsherdComponent, IntroComponent, SampleComponent],
-  imports: [BrowserModule, AppRoutingModule, NgZorroAntdModule, FormsModule, HttpClientModule, BrowserAnimationsModule, IconsProviderModule],
+  declarations: [
+    AppComponent,
+    ShepsherdComponent,
+    IntroComponent,
+    SampleComponent,
+    LoginComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+  ],
   providers: [ShepherdService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
